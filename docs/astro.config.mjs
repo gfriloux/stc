b210c8614717cf9e1aaac7ed7b37175997dff22e
Guillaume Friloux <guillaume@friloux.me>
2026-05-30 18:40:10 +0200

    chore(ci): add CI
    add CI
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://gfriloux.github.io',
+  base: '/stc',
   integrations: [
     starlight({
       title: 'STC',
